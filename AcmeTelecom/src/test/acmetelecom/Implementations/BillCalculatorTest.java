8a117dcb10c3c819ee53ee19ea3165f99a4e1a02
lzhou <peter.liyi.z@gmail.com>
2013-11-20 06:24:17 -0800

    unit testing initial framework

---
+++ AcmeTelecom/src/test/acmetelecom/Implementations/BillCalculatorTest.java
@@ -0,0 +1,53 @@
+package test.acmetelecom.Implementations;
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import java.math.BigDecimal;
+import com.acmetelecom.Implementations.BillCalculator;
+import com.acmetelecom.call.Call;
+import com.acmetelecom.call.CallEnd;
+import com.acmetelecom.call.CallStart;
+import com.googlecode.zohhak.api.Coercion;
+import com.googlecode.zohhak.api.TestWith;
+import com.googlecode.zohhak.api.runners.ZohhakRunner;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeUtils;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+@RunWith(ZohhakRunner.class)
+public class BillCalculatorTest {    
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@TestWith({
+		   "'11/11/2013 19:00:00', '12/11/2013 06:59:59', 20, 10, 431990"
+		})
+	public void testCalculate (String startTime, String endTime, BigDecimal peakRate, BigDecimal offPeakRate, BigDecimal expectedResult) {
+		DateTimeFormatter formatter = DateTimeFormat.forPattern("dd/MM/yyyy HH:mm:ss");
+		freeze(formatter.parseDateTime(startTime));
+		CallStart startEvent = new CallStart("Caller", "Callee");
+		freeze(formatter.parseDateTime(endTime));
+		CallEnd endEvent = new CallEnd("Caller", "Callee");
+		unfreeze();
+		Call call = new Call(startEvent, endEvent);		
+		BillCalculator tester = new BillCalculator();
+	    BigDecimal result = tester.calculate(call, peakRate, offPeakRate);
+	    assertTrue(result.equals(expectedResult));
+	}
+	
+	@Coercion
+	public BigDecimal whateverName (String input) {
+		return new BigDecimal(input);
+	}
+    private void freeze(DateTime frozenDateTime) {DateTimeUtils.setCurrentMillisFixed(frozenDateTime.getMillis());}
+    private void unfreeze() {DateTimeUtils.setCurrentMillisSystem();}
+
+}
